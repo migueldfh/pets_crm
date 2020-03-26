6ea4fb399e7db36324791d60a560dddb829065d2
miguel.de.la.fuente <miguel.dlafuenteh@gmail.com>
2020-03-26 15:16:46 -0600

    Added two more fields to client model.

--- app/Client.php
+++ app/Client.php
@@ -15,6 +15,6 @@ class Client extends Model
       'seller_id', 'subsidiary_id', 'type', 'person_type', 'category','name','rfc','street','ext_number',
       'neighborhood','city','state','zipcode','country','currency','photo','owner','vet',
       'admon','discount','credit','max_days_credit','credit_limit','max_credit_limit',
-      'account_holder','account_number','bank_name',
+      'account_holder','account_number','bank_name', 'telvet', 'responsable'
     ];
 }
